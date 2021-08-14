495f1e833a53e0bff2af749669c70bf9b9f7b1b9
dhino12 <dhinorahmad0@gmail.com>
2021-08-14 18:42:26 +0700

    fix double quote replace with single

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import "@fortawesome/fontawesome-free/js/fontawesome.min";
-import "@fortawesome/fontawesome-free/js/brands";
-import "@fortawesome/fontawesome-free/js/solid";
-import "./style/style.css";
-import "./script/component/tab-indicators.js"
-import "./script/component/navigation/nav-container.js";
-import "./script/component/header/header";
-import "./script/component/detailCharacter/profile-chara.js";
-import "./script/component/detailCharacter/skillCarousel/list-skill-talent.js";
-import "./script/component/detailCharacter/constellation/list-card-grid";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/js/fontawesome.min';
+import '@fortawesome/fontawesome-free/js/brands';
+import '@fortawesome/fontawesome-free/js/solid';
+import './style/style.css';
+import './script/component/tab-indicators';
+import './script/component/navigation/nav-container';
+import './script/component/header/header';
+import './script/component/detailCharacter/profile-chara';
+import './script/component/detailCharacter/skillCarousel/list-skill-talent';
+import './script/component/detailCharacter/constellation/list-card-grid';
 
-import main from "./script/view/main.js";
+import main from './script/view/main';
 
 const locationLink = window.location.pathname;
 
-if(locationLink === "/"  || locationLink === "/index.html"){
-    document.addEventListener("DOMContentLoaded", main);
+if (locationLink === '/' || locationLink === '/index.html') {
+    document.addEventListener('DOMContentLoaded', main);
 }
