db10b2ef82ee2b03be06679f4cf3a2dad3771466
dhino12 <dhinorahmad0@gmail.com>
2021-08-14 17:13:26 +0700

    checkpoint

--- webpack.common.js
+++ webpack.common.js
@@ -10,26 +10,38 @@ module.exports = {
         path: path.resolve(__dirname, "dist"),
         filename: "[name].js"
     },
+    optimization: {
+        splitChunks: {
+            chunks: "all"
+        }
+    },
     module: {
-        rules: [{
-            test: /\.css$/,
-            use: [{
-                    loader: "style-loader"
-                },
-                {
-                    loader: "css-loader"
-                }
-            ]
-        }]
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: "style-loader"
+                    },
+                    {
+                        loader: "css-loader"
+                    }
+                ]
+            }
+        ]
     },
     plugins: [
         new HtmlWebpackPlugin({
+            inject: true,
             template: "./src/page/character.html",
-            filename: "index.html"
+            filename: "index.html",
+            chunks: ["index"]
         }),
         new HtmlWebpackPlugin({
+            inject: true,
             template: "./src/page/artifacts.html",
-            filename: "artifacts.html"
+            filename: "artifacts.html",
+            chunks: ["artifacts"]
         })
     ]
 }
